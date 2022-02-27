6d8fb7905ec032b900e85d21042b197dea43363b
Thomas <thomas.mennour@ens.uvsq.fr>
2022-02-27 16:19:49 +0100

    modification par thomas

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -6,3 +6,24 @@
 #      GAEL
 # https://github.com/uvsq22101081/projet_tas_de_sable.git
 #########################################
+
+#import des modules
+
+import random as rd
+
+# variables globales
+
+config = []
+
+# fonctions
+
+def config_vide() :
+    global config
+    config = [[0] * 3 for i in range(3)]
+
+
+def config_aléatoire():
+    for i in range(3) :
+        for j in range(3) :
+            nbr_grains = rd.randint(0,3)
+            config[i][j] += nbr_grains
