27e127a3f752cfba1addbb55e83f80492525e841
Yasmine ABALIL <yasmine.abalil@ens.uvsq.fr>
2022-03-08 15:00:44 +0100

    Création de la fenetre + de la fonction d'affichage de la grille + modifications légères + attribution des fonctions au boutons

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,14 +2,15 @@
 # groupe MI 3
 # ABALIL YASMINE  
 # MENNOUR THOMAS  
-# GOD ELIES
-#      GAEL
+# ELIES ROMAIN
+# PEMBELE GAEL
 # https://github.com/uvsq22101081/projet_tas_de_sable.git
 #########################################
 
 #import des modules
 
 import random as rd
+import tkinter as tk
 
 # variables globales
 
@@ -20,10 +21,58 @@ config = []
 def config_vide() :
     global config
     config = [[0] * 3 for i in range(3)]
+    
 
 
 def config_aléatoire():
+    config_vide()
     for i in range(3) :
         for j in range(3) :
-            nbr_grains = rd.randint(0,3)
+            nbr_grains = rd.randint(0,9)
             config[i][j] += nbr_grains
+
+def affichage_grille():
+    
+    x = 0
+    y =0
+    canvas.delete("all")
+    for i in range(len(config)):
+        x +=140
+        for j in range(len(config[i])):
+            y +=140
+            canvas.config(bg="white")
+            canvas.create_rectangle(0, 0, x, y)
+            canvas.create_text( (x-HAUTEUR_GRILLE), (y-LARGEUR_GRILLE), text =str(config[i][j]), width=70)
+        y=0
+
+
+
+def générer_aléatoirement():
+    config_aléatoire()
+    affichage_grille()
+
+
+def générer_vide():
+    config_vide()
+    affichage_grille()
+HAUTEUR_CANEVAS,LARGEUR_CANEVAS = 420,420
+HAUTEUR_GRILLE, LARGEUR_GRILLE= 70,70
+
+
+
+    
+#CREATION DE LA FENETRE#
+
+racine=tk.Tk()
+racine.title("Ecoulement d'un tas de sable")
+canvas = tk.Canvas(racine, height= HAUTEUR_CANEVAS, width=LARGEUR_CANEVAS,  bg="grey")
+boutonaléatoire =tk.Button(racine, text="Génération aléatoire", command= générer_aléatoirement)
+boutonvide =tk.Button(racine, text="Génération vide", command=générer_vide)
+boutoncommencer =tk.Button(racine, text="Commencement")
+canvas.grid(row=1, column=0, columnspan=3)
+boutonaléatoire.grid(row=2, column=0)
+boutoncommencer.grid(row=2, column=1)
+boutonvide.grid(row=2, column=2)
+racine.mainloop()
+
+
